907fc6bbd87d00efa5553c17f2570c661362618b
sgaona21 <88602650+sgaona21@users.noreply.github.com>
2025-05-01 21:17:54 -0700

    created event listener for employee cards

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function getUsersTESTER() {
     fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
-    .then(users => console.log(users.results[0]))
+    .then(employees => console.log(employees.results[0]))
 }
 
 getUsersTESTER()
@@ -12,27 +12,29 @@ const galleryContainer = document.getElementById('gallery');
 async function getUsers() {
     try {
         let response = await fetch('https://randomuser.me/api/?results=12');
-        let users = await response.json();
-        displayUsers(users)
+        let employees = await response.json();
+        displayUserCards(employees)
     } catch (error) {
         console.log(error);
         alert("Failed to load employee data (Sorry)");
     }
 }
 
-function displayUsers(users) {
-    for (let i = 0; i < users.results.length; i++) {
+
+
+function displayUserCards(employees) {
+    for (let i = 0; i < employees.results.length; i++) {
         let cardContainer = createNewElement('div', ['card']);
         let cardThumbnailContainer = createNewElement('div', ['card-img-container']);
         let cardThumbnail = createNewElement('img', ['card-img']);
-        cardThumbnail.src = users.results[i].picture.large;
+        cardThumbnail.src = employees.results[i].picture.large;
         let cardInfoContainer = createNewElement('div', ['card-info-container']);
         let cardNameH3 = createNewElement('h3', ['card-name', 'cap'], 'name');
-        cardNameH3.textContent = `${users.results[i].name.first} ${users.results[i].name.last}`;    
+        cardNameH3.textContent = `${employees.results[i].name.first} ${employees.results[i].name.last}`;    
         let cardEmail = createNewElement('p', ['card-text']);
-        cardEmail.textContent = users.results[i].email;
+        cardEmail.textContent = employees.results[i].email;
         let cardLocation = createNewElement('p', ['card-text', 'cap']);
-        cardLocation.textContent = `${users.results[i].location.city}, ${users.results[i].location.state}`;
+        cardLocation.textContent = `${employees.results[i].location.city}, ${employees.results[i].location.state}`;
 
         cardThumbnailContainer.appendChild(cardThumbnail);
         cardInfoContainer.append(cardNameH3, cardEmail, cardLocation);
@@ -41,14 +43,35 @@ function displayUsers(users) {
     }
 }
 
+function displayEmployeeModal() {
+
+}
+
+
+
+
+
+
 //Helper Functions 
 function createNewElement(elementType, classNames = [], id) {
     let newElement = document.createElement(elementType);
     newElement.classList.add(...classNames);
     newElement.id = id;
 
-    return newElement
+    return newElement;
 }
+
+
+
+
+galleryContainer.addEventListener('click', (e) => {
+    const card = e.target.closest('.card');
+    if (card) {
+        console.log('HEYYYY lol')
+    }
+})
+
+
 
 
 getUsers();
